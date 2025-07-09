684f85268e280c895149964dd160469217fa7cf7
Piyash1 <moniruzzamanpiyash@gmail.com>
2025-07-10 05:12:45 +0600

    login api request

--- backend-django/accounts/serializers.py
+++ backend-django/accounts/serializers.py
@@ -3,15 +3,25 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8, style={'input_type': 'password'})
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
-        
+
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("Username is already taken.")
+        return value
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email is already registered.")
+        return value
+
     def create(self, validated_data):
         user = User.objects.create_user(
             validated_data['username'],
             validated_data['email'],
             validated_data['password']
         )
-        # user = user.objects.create_user(**validated_data)
         return user
--- backend-django/stock_prediction_main/settings.py
+++ backend-django/stock_prediction_main/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'accounts',
     'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -52,7 +53,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True  # For development only
 
 ROOT_URLCONF = 'stock_prediction_main.urls'
 
