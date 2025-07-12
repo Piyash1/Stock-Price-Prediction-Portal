86b2347efe0f7f99601ec2bcd203ee0ac7917954
Piyash1 <moniruzzamanpiyash@gmail.com>
2025-07-12 21:52:53 +0600

    login functionality

--- backend-django/api/urls.py
+++ backend-django/api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from accounts import views as Userviews
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('register/', Userviews.Register_view.as_view()),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
